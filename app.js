3bdbf0c7eb9e35ddff63cc23078158c0bb27df0a
Doog <63171864+doogwebdevgittutorial@users.noreply.github.com>
2020-11-25 15:49:32 -0600

    add sample async functions

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-ethereum.autoRefreshOnNetworkChange = false;
+if(ethereum) {
+  ethereum.autoRefreshOnNetworkChange = false;
+  ethereum.enable();
+} else {
+  alert("You need a MetaMask account to experience this site.")
+}
 
-ethereum.enable();
 const provider = new ethers.providers.Web3Provider(window.ethereum);
 let signer = provider.getSigner();
 const contractAddress = "0xb571B3ea89bDF2FBCf08E737432730C7e7ceD798";
@@ -10,7 +14,8 @@ const contractABI = [
   "function tokenOfOwnerByIndex(address owner, uint256 index) public view returns (uint256)",
   "function totalSupply() public view returns (uint256)",
   "function tokenByIndex(uint256 index) public view returns (uint256)",
-  "function safeTransferFrom(address from, address to, uint256 tokenId) public"
+  "function safeTransferFrom(address from, address to, uint256 tokenId) public",
+  "function ownerOf(uint256 tokenId) public view returns (address)"
 ];
 
 const contract = new ethers.Contract(contractAddress, contractABI, provider);
@@ -21,6 +26,30 @@ let address;
 let totalSupply;
 let existingTokens = []; // every single color token that has been minted
 let signerTokens = []; // every color token owned by the signer
+
+
+main();
+
+async function main() {
+
+  address = await signer.getAddress();
+  console.log(address);
+
+  totalSupply = await contract.totalSupply();
+  console.log(totalSupply);
+
+  let tokenNumberOne = await contract.tokenByIndex(0);
+  tokenNumberOne = +tokenNumberOne
+  console.log(tokenNumberOne);
+
+  let ownerOfTokenOne = await contract.ownerOf(tokenNumberOne);
+  console.log(ownerOfTokenOne);
+
+  let myBalance = await contract.balanceOf(address);
+  myBalance = +myBalance;
+  console.log(myBalance);
+}
+
 
 
 
