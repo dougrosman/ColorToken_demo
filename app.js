1b5c69055b7701576bbb1b0719ea859d7aa1ec20
Doog <63171864+doogwebdevgittutorial@users.noreply.github.com>
2020-11-25 19:45:12 -0600

    add up to minting new tokens

--- app.js
+++ app.js
@@ -28,26 +28,176 @@ let existingTokens = []; // every single color token that has been minted
 let signerTokens = []; // every color token owned by the signer
 
 
+$('#generate-color').click(async function(){
+  await mintColor();
+})
+
 main();
 
 async function main() {
-
   address = await signer.getAddress();
-  console.log(address);
+  console.log("Current MetaMask Wallet: " + address);
+  $("#address").text(address);
 
+  await displayOwnedColors(address);  
+}
+
+async function displayOwnedColors(_address) {
+  let balance = await contract.balanceOf(_address);
+  balance = +balance;
+
+  // an array of all the ColorToken Ids owned by the current user
+  signerTokens = await getColorsByOwner(address, balance);
+
+  // an arry of colors (as JavaScript objects) that are owned by the current user
+  let ownedColors = idsToColor(signerTokens);
+
+  // loop through all the colors;
+  for(let i = 0; i < ownedColors.length; i++) {
+    let t = ownedColors[i];
+
+    let colorId = "token" + i;
+
+    // add the color token to the web page
+    let colorDiv =
+    `
+    <div class="color-token" id="${colorId}">
+      <div class="color-token__tile"></div>
+      <div class="color-token__text">
+        <div class="color-token__num">${i + 1}</div>
+        <div class="color-token__hex">#abcdef</div>
+      </div>
+    </div>
+    `
+
+    $('.color-gallery').append(colorDiv);
+
+    $(`#${colorId}`).children('.color-token__tile').css("background", `rgb(${t.r}, ${t.g}, ${t.b})`);
+    $(`#${colorId}`).children('.color-token__text').children(".color-token__hex").text(`${rgbToHex(t.r, t.g, t.b)}`);
+  }
+}
+
+async function getColorsByOwner(_address, _balance) {
+  let ids = [];
+  for(let i = 0; i < _balance; i++) {
+    let currToken = await contract.tokenOfOwnerByIndex(_address, i);
+    currToken = +currToken;
+    ids.push(currToken);
+  }
+  return ids.sort();
+}
+
+function idsToColor(_ids) {
+
+  let colors = [];
+
+  for(let i = 0; i < _ids.length; i++) {
+    // convert ID to a string
+    let id = "" + _ids[i];
+
+    // extract each color from the ID (e.g. 1255000255)
+    let r = parseInt(id.substr(1, 3)); // index 1, 2, 3 == 255
+    let g = parseInt(id.substr(4, 3)); // index 4, 5, 6 == 000
+    let b = parseInt(id.substr(7, 3)); // index 7, 8, 9 == 255
+    let color = {r: r, g: g, b: b};
+
+    colors.push(color);
+  }
+  return colors;
+}
+
+// function to convert a decimal number to a hex number
+function componentToHex(c) {
+  let hex = c.toString(16);
+  return hex.length == 1 ? "0" + hex : hex;
+}
+
+// function to convert an RGB color to Hex color
+function rgbToHex(r, g, b) {
+  return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+
+async function mintColor() {
+
+  let timeOut;
+  let interval = 250;
+  let cycle = true;
+
+  $('#loading').show();
+  $('#generate-color').hide();
+
+  setInterval(function(){
+    if(cycle) {
+      let randR = Math.floor(Math.random()*256);
+      let randG = Math.floor(Math.random()*256);
+      let randB = Math.floor(Math.random()*256);
+      $('.minted-color__tile').css("background", `rgb(${randR}, ${randG}, ${randB})`);
+      $('.minted-color__hex').text(`${rgbToHex(randR, randG, randB)}`);
+    }
+  }, interval);
+
+  if(existingTokens.length == 0) {
+    existingTokens = await loadExisting();
+    timeOut = 2000;
+  } else {
+    timeOut = 10000;
+  }
+
+  setTimeout(function(){
+    cycle = false;
+    $('#loading').hide();
+    $('#generate-color').show();
+
+    let rewardId = checkExisting(existingTokens);
+    let id = "" + rewardId;
+
+    let r = parseInt(id.substr(1, 3));
+    let g = parseInt(id.substr(4, 3));
+    let b = parseInt(id.substr(7, 3));
+
+    $('.minted-color__tile').css("background", `rgb(${r}, ${g}, ${b})`);
+    $('.minted-color__hex').text(`${rgbToHex(r, g, b)}`);
+
+    tokenWithSigner.awardItem(address, rewardId);
+  }, timeOut)
+}
+
+// load all pre-existing tokens into an array (existingTokens)
+async function loadExisting() {
+
+  // get the total supply of existing tokens
   totalSupply = await contract.totalSupply();
-  console.log(totalSupply);
+  totalSupply = +totalSupply;
 
-  let tokenNumberOne = await contract.tokenByIndex(0);
-  tokenNumberOne = +tokenNumberOne
-  console.log(tokenNumberOne);
+  // loop through all existing tokens and add them to the existingTokens array
+  for(let i = 0; i < totalSupply; i++) {
+    let currToken = await contract.tokenByIndex(i);
+    existingTokens.push(+currToken);
+  }
+  return existingTokens;
+}
 
-  let ownerOfTokenOne = await contract.ownerOf(tokenNumberOne);
-  console.log(ownerOfTokenOne);
+// generates random colors until a unique (unowned) color is generated
+function checkExisting(_existingTokens) {
 
-  let myBalance = await contract.balanceOf(address);
-  myBalance = +myBalance;
-  console.log(myBalance);
+  // generate an initial random color
+  let randR = Math.floor(Math.random()*256);
+  let randG = Math.floor(Math.random()*256);
+  let randB = Math.floor(Math.random()*256);
+
+  // combine random RGB values into a single number
+  let myColor = 1000000000 + randR * 1000000 + randG * 1000 + randB;
+
+  while(_existingTokens.indexOf(myColor) > 0) {
+    let randR = Math.floor(Math.random()*256);
+    let randG = Math.floor(Math.random()*256);
+    let randB = Math.floor(Math.random()*256);
+
+    myColor = 1000000000 + randR * 1000000 + randG * 1000 + randB;
+  }
+
+  console.log("Minted Color: " + myColor);
+  return myColor;
 }
 
 
